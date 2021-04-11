f84d8551abf3dc909dd94f93da15032e7ac9b324
SpicyChickenFLY <xixiayuchao@gmail.com>
2021-04-11 22:31:51 +0800

    improve build && run script

--- build/build.sh
+++ build/build.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
+# initialize database
 mysql -uroot -p -D never_todo < todo.sql
+
+# move linux service conf
+cp ./never-todo-backend.service
+
+# compile go program
+go build ../main.go
+
+# move executable file to /usr/bin
+mv 
+
+# move static file to /usr/bin
+mv 
